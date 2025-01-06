b184a4cb3875c25b2d3fa808296dd5a51f3617bd
Lakmal-dot <lakmalchathuranga13@gmail.com>
2025-01-06 23:31:06 +0530

    feat: initial commit

---
+++ Singleton-desing-pattarn-application/Singleton-desing-pattarn-application/Program.cs
@@ -0,0 +1,19 @@
+
+using System;
+using Singleton_desing_pattarn_application;
+
+Thread t1 = new Thread(() =>
+{
+    var instance = UploadService.Instance(1);
+});
+
+Thread t2 = new Thread(() =>
+{
+    var instance = UploadService.Instance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
---
+++ Singleton-desing-pattarn-application/Singleton-desing-pattarn-application/UploadService.cs
@@ -0,0 +1,31 @@
+﻿namespace Singleton_desing_pattarn_application
+{
+    sealed class UploadService
+    {
+        private UploadService() {}
+
+        public int Id { get; set; }
+
+        private static UploadService _instance;
+        private static readonly object _lock = new object();
+        
+        public static UploadService Instance(int id)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new UploadService();
+                        _instance.Id = id;
+                    }
+                }
+                
+            }
+
+            return _instance;
+        }
+        
+    }
+}
